292bbea15ec3ed0cae831fcc07558483dab925f0
BN-Mara <73875731+BN-Mara@users.noreply.github.com>
2021-04-28 18:38:56 +0100

    second3

--- src/component/theme.js
+++ src/component/theme.js
@@ -3,6 +3,7 @@ import { Button, Container, Row, Col, Card, Image, Nav } from 'react-bootstrap';
 import './theme.css';
 import bgimg from '../img/mainimg.jpg';
 import imgt from '../img/eating.jfif';
+import bgdiv from '../img/imgdiv.jpg';
 
 
 export default class Theme extends Component{
@@ -49,6 +50,7 @@ according to the package subscribed
                    <Nav
                         activeKey="/home"
                          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+                         
                     >
                     <Nav.Item>
                      <Nav.Link href="/home" className="white">home</Nav.Link>
@@ -87,13 +89,10 @@ according to the package subscribed
                 <div class="d-flex justify-content-center">
                 <h2 className="center margett">OUR RESTAURANTS</h2>
                 </div>
-               
-  
-               
-                    
+                                                
                     <Row>
                         <Col lg={3}>
-                        <Image src={imgt} width="300" height="450" thumbnail/>
+                        <Image src={imgt} width="300" height="500" thumbnail/>
                         <spam>Mama Eva Restaurant</spam>
                         <br/>
                         Nyamalango<br/>
@@ -129,6 +128,144 @@ according to the package subscribed
                 </div>
 
                 </Container>
+                <div className="bgdark">
+
+                    <div className="marge margetb">
+                    <div class="d-flex justify-content-center">
+                        <p>
+                        <h2 className="center">OUR PRICING</h2>
+                        </p>
+                        <p style={{textAlign:"center"}}><b>Pick from 7 different packages</b><br/>
+                        Straight forward services, transparent pricing.<br/>
+                        No hidden cost .No extra fees</p>
+                    </div>
+                        <Row >
+                            <Col lg>
+                        <Card >
+                        <Card.Header>
+                        <Card.Title>Basic</Card.Title>
+
+                        </Card.Header>
+                            <Card.Body>
+                            
+                        <Card.Text>
+                             <h2>Twice per day</h2>
+                             <p>
+                                 Breakfast or lunch
+                             </p>
+                             <p>
+                                 Lunch or dinner
+                             </p>
+                             <h1>
+                                 Tsh 2500
+                                 </h1>
+                    </Card.Text>
+                    <Button variant="danger" size="sm">Book Now</Button>
+                    </Card.Body>
+                    </Card>
+
+                            </Col>
+                            <Col lg>
+                            <Card >
+                        <Card.Header>
+                        <Card.Title>Basic</Card.Title>
+
+                        </Card.Header>
+                            <Card.Body>
+                            
+                        <Card.Text>
+                             <h2>Twice per day</h2>
+                             <p>
+                                 Breakfast or lunch
+                             </p>
+                             <p>
+                                 Lunch or dinner
+                             </p>
+                             <h1>
+                                 Tsh 2500
+                                 </h1>
+                    </Card.Text>
+                    <Button variant="danger" size="sm">Book Now</Button>
+                    </Card.Body>
+                    </Card>
+                            </Col>
+                            
+                            <Col lg>
+                            <Card >
+                        <Card.Header>
+                        <Card.Title>Basic</Card.Title>
+
+                        </Card.Header>
+                            <Card.Body>
+                            
+                        <Card.Text>
+                             <h2>Twice per day</h2>
+                             <p>
+                                 Breakfast or lunch
+                             </p>
+                             <p>
+                                 Lunch or dinner
+                             </p>
+                             <h1>
+                                 Tsh 2500
+                                 </h1>
+                    </Card.Text>
+                    <Button variant="danger" size="sm">Book Now</Button>
+                    </Card.Body>
+                    </Card>
+                            </Col>
+                            <Col lg>
+                            <Card >
+                        <Card.Header>
+                        <Card.Title>Basic</Card.Title>
+
+                        </Card.Header>
+                            <Card.Body>
+                            
+                        <Card.Text>
+                             <h2>Twice per day</h2>
+                             <p>
+                                 Breakfast or lunch
+                             </p>
+                             <p>
+                                 Lunch or dinner
+                             </p>
+                             <h1>
+                                 Tsh 2500
+                                 </h1>
+                    </Card.Text>
+                    <Button variant="danger" size="sm">Book Now</Button>
+                    </Card.Body>
+                    </Card>
+                            </Col>
+                            <Col lg>
+                            <Card >
+                        <Card.Header>
+                        <Card.Title>Basic</Card.Title>
+
+                        </Card.Header>
+                            <Card.Body>
+                            
+                        <Card.Text>
+                             <h2>Twice per day</h2>
+                             <p>
+                                 Breakfast or lunch
+                             </p>
+                             <p>
+                                 Lunch or dinner
+                             </p>
+                             <h1>
+                                 Tsh 2500
+                                 </h1>
+                    </Card.Text>
+                    <Button variant="danger" size="sm">Book Now</Button>
+                    </Card.Body>
+                    </Card>
+                            </Col>
+                        </Row>
+                    </div>
+
+                </div>
             </>
 
         );
